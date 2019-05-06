89777e858fc8b0fbe004feeb75c963c0b7b88c6f
Fernando Alamillo <falamillo88@gmail.com>
2019-05-06 10:22:11 +0100

    Updates

---
+++ precache-manifest.803f33f98df65006e0cd001850933da0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc259ac497be1ee29f1f2a74ca486a74",
+    "url": "/sw-game/index.html"
+  },
+  {
+    "revision": "d3ab6a57a3ae346b9e58",
+    "url": "/sw-game/static/css/main.f56728f9.chunk.css"
+  },
+  {
+    "revision": "f2c6513e39cdc288baae",
+    "url": "/sw-game/static/js/2.f4f84f3f.chunk.js"
+  },
+  {
+    "revision": "d3ab6a57a3ae346b9e58",
+    "url": "/sw-game/static/js/main.4f2da570.chunk.js"
+  },
+  {
+    "revision": "75143c677497b7351588",
+    "url": "/sw-game/static/js/runtime~main.a6984e46.js"
+  }
+]);
